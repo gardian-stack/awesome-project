2ab68c373c29c74519ad2869f2db79fa8f897633
gardian-stack <48905929+gardian-stack@users.noreply.github.com>
2020-05-15 11:25:53 +0700

    delete cache

--- src/index.js
+++ src/index.js
@@ -1,25 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
 import * as serviceWorker from './serviceWorker';
 import HelloComponent from './component/HelloComponent';
 import StateFullComponent from './container/StateFullComponent';
-// import YouTubeComp from './component/YouTubeComp/YouTubeComp';
-import Main from './container/layout/main';
 import Content from './component/newTest/Content';
 import BlogPost from './component/blogPost/BlogPost';
 
-// ReactDOM.render(<HelloComponent />, document.getElementById('root'));
-
-// ReactDOM.render(<StateFullComponent />, document.getElementById('root2'));
-
-ReactDOM.render(<Main />, document.getElementById('root'));
-// ReactDOM.render(<Content />, document.getElementById('root1'));
-// ReactDOM.render(<BlogPost />, document.getElementById('root2'));
-
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
